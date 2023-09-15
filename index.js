22b3c58950272d45d095dc8ff35868ec12e29f15
geetika <102815937+geetikaofficial@users.noreply.github.com>
2023-09-15 12:22:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// fetch('data.json') 
+//   .then(response => response.json())
+//   .then(data => {
+//     // Handle the JSON data here
+//   data.forEach(item=>{
+//     console.log
+//   })
+//     console.log(data); // You can access the JSON data as 'data' in this scope
+//   })
+//   .catch(error => {
+//     console.error('Error loading JSON:', error);
+//   });
